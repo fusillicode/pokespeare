fcd1618aa7e93bb3c019c858542b1fba485542cf
fusillicode <fusillicode@gmail.com>
2020-11-21 12:32:36 +0100

    Extract PokeApi client in separate module

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 mod log_helpers;
+mod poke_api_client;
 
 use actix_slog::StructuredLogger;
 use actix_web::middleware::Compress;
 use actix_web::web::{Data, Path};
 use actix_web::{get, App, Error, HttpResponse, HttpServer};
 use log_helpers::*;
-use rand::prelude::*;
+use poke_api_client::PokeApiClient;
 use reqwest::Url;
 use serde::{Deserialize, Serialize};
 
@@ -49,11 +50,6 @@ async fn main() -> std::io::Result<()> {
 }
 
 #[derive(Clone)]
-struct PokeApiClient {
-    endpoint: Url,
-}
-
-#[derive(Clone)]
 struct FunTranslationsClient {
     endpoint: Url,
 }
@@ -65,38 +61,18 @@ async fn get_shakesperean_description(
     fun_translations_client: Data<FunTranslationsClient>,
     pokemon_id_or_name: Path<String>,
 ) -> Result<HttpResponse, Error> {
-    let mut poke_api_species_request_url = poke_api_client.endpoint.clone();
-    poke_api_species_request_url
-        .path_segments_mut()
-        .unwrap()
-        .extend(&["pokemon-species", &pokemon_id_or_name]);
     let mut fun_translations_shakespere_request_url = fun_translations_client.endpoint.clone();
     fun_translations_shakespere_request_url
         .path_segments_mut()
         .unwrap()
         .push("shakespeare.json");
 
-    let pokemon_species_response = reqwest::get(poke_api_species_request_url).await;
-
-    info!(log, "PokeApi response"; "response" => ?pokemon_species_response);
-
-    let pokemon_species = pokemon_species_response
-        .unwrap()
-        .json::<PokemonSpecies>()
+    let cleaned_random_en_description = poke_api_client
+        .get_random_description(&pokemon_id_or_name)
         .await
         .unwrap();
 
-    let en_descriptions = pokemon_species
-        .descriptions
-        .iter()
-        .filter(|d| d.language.name == "en");
-
-    let mut rng = rand::thread_rng();
-    let random_en_description = en_descriptions.choose(&mut rng).unwrap();
-    let cleaned_random_en_description = random_en_description
-        .text
-        .replace('\n', " ")
-        .replace("\\u000", "");
+    info!(log, "PokeApi data"; "pokemon_description" => ?cleaned_random_en_description);
 
     let shakesperean_description_response = reqwest::Client::new()
         .get(fun_translations_shakespere_request_url)
@@ -113,24 +89,6 @@ async fn get_shakesperean_description(
         .unwrap();
 
     Ok(HttpResponse::Ok().json(shakesperean_description.contents.translated_text))
-}
-
-#[derive(Deserialize, Serialize)]
-struct PokemonSpecies {
-    #[serde(rename = "flavor_text_entries")]
-    descriptions: Vec<PokemonDescription>,
-}
-
-#[derive(Deserialize, Serialize)]
-struct PokemonDescription {
-    #[serde(rename = "flavor_text")]
-    text: String,
-    language: Language,
-}
-
-#[derive(Deserialize, Serialize)]
-struct Language {
-    name: String,
 }
 
 #[derive(Deserialize, Serialize)]
---
+++ src/poke_api_client.rs
@@ -0,0 +1,60 @@
+use rand::prelude::*;
+use reqwest::Url;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone)]
+pub struct PokeApiClient {
+    pub endpoint: Url,
+}
+
+#[derive(Deserialize, Serialize)]
+struct PokemonSpecies {
+    #[serde(rename = "flavor_text_entries")]
+    descriptions: Vec<PokemonDescription>,
+}
+
+#[derive(Deserialize, Serialize)]
+struct PokemonDescription {
+    #[serde(rename = "flavor_text")]
+    text: String,
+    language: Language,
+}
+
+#[derive(Deserialize, Serialize)]
+struct Language {
+    name: String,
+}
+
+impl PokeApiClient {
+    pub async fn get_random_description(
+        &self,
+        pokemon_id_or_name: &str,
+    ) -> Result<String, Box<dyn std::error::Error>> {
+        let mut poke_api_species_request_url = self.endpoint.clone();
+        poke_api_species_request_url
+            .path_segments_mut()
+            .map_err(|_| "Can't construct pokemon-species API URL")?
+            .extend(&["pokemon-species", &pokemon_id_or_name]);
+
+        let pokemon_species = reqwest::get(poke_api_species_request_url.clone())
+            .await?
+            .json::<PokemonSpecies>()
+            .await?;
+
+        let language_filter = "en";
+        Ok(pokemon_species
+            .descriptions
+            .iter()
+            .filter(|d| d.language.name == language_filter)
+            .choose(&mut rand::thread_rng())
+            .ok_or_else(|| {
+                format!(
+                    "No '{}' descripiton found when calling PokeApi URL {:?}",
+                    language_filter, poke_api_species_request_url
+                )
+            })?
+            .text
+            .replace('\n', " ")
+            .replace("\\u000", ""))
+    }
+}
